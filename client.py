49c8374f5487fa1728c504619efba19cd6e5e7b6
Brady Welsh <blwelsh@calpoly.edu>
2026-05-18 14:17:59 -0700

    updated logic to include locks, and handle timeout with those locks. also initialized server RTT logic using socket timeout

--- client.py
+++ client.py
@@ -11,15 +11,17 @@ soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = "localhost"
 port = 5000
 
-try:
-    soc.bind((host, port))
-except:
-    print("Bind failed. Error : " + str(sys.exc_info()))
-    sys.exit()
+lock = threading.Lock()
 
 
 # read 1MB file
 
+def calc_checksum(databytes):
+    sum = 0
+    for byte in databytes:
+        sum += byte
+    res = sum % 65535
+    return res
 
 # congestion control mechanisms
 
@@ -40,56 +42,122 @@ def send_chunk(chunk, sequence_num, checksum):
     # 4 byte sequence num
     # 2 byte checksum
     # 1024 byte data or less
-    # TODO: if last chunk, shouldn't be 1024 bytes
 
-    message = struct.pack("ih1024s", sequence_num, checksum, chunk)
+    message = struct.pack("iH1024s", sequence_num, checksum, chunk)
 
     soc.sendto(message, (host, port))
     return
 
 # handle timeouts:
 
-timeout = 10
+timeout = 0.5
 
 # for chunks that need to be retransmitted
 retransmit = []
 
-# thread for handling server responses
-
-
 
 with open("gistfile1.txt", "rb") as file:
+    # most recently received ACK
+    next_sequence = 0
 
+
+    send_base = 0
+    next_to_send = 0
+    cwnd = 1
+    ssthresh = float("inf")
+    dup_ack_count = 0
+    last_ack = 0
+
+    # thread for receiving acks from server
     def handle_ack():
+        global send_base, cwnd, ssthresh, dup_ack_count, last_ack, next_to_send
+
         while True:
             # receieve ACKs
-            # TODO: how big should server response be
-            ack, address = soc.recvfrom(4)
+            retransmit_packet = None
+            message, address = soc.recvfrom(4)
 
-            if not isinstance(ack, int):
-                print("ack not an int... :(")
-                continue
+            ack = struct.unpack("i", message)[0]
 
-            ack_time = datetime.datetime.now()
-            if (ack_time - unreceived_acks[ack - 1]).total_seconds() < timeout:
-                retransmit.append(ack - 1)
-                pass
+            with lock:
+                print("received new ack :) from", ack)
+
+
+                if not isinstance(ack, int):
+                    print("ack not an int... :(")
+                    continue
+
+                if ack > send_base:
+                    for seq in range(send_base, ack):
+                        unreceived_acks.pop(seq, None)
+
+                    send_base = ack
+                    last_ack = ack
+                    dup_ack_count = 0
+
+                    #slow start:
+                    if cwnd < ssthresh:
+                        cwnd += 1
+                    # congestion avoidance:
+                    else:
+                        cwnd += 1 / cwnd
+
+                # duplicate ack/fast retransmit
+                elif ack == last_ack:
+                    dup_ack_count += 1
+
+                    if dup_ack_count == 3:
+                        missing_packet = ack
+                        print("Fast retransmit " + str(missing_packet))
+
+                        retransmit_packet = ack
+                        ssthresh = max(cwnd/2, 1)
+                        cwnd = ssthresh + 3
+
+            if retransmit_packet is not None:
+                send_chunk(chunks[retransmit_packet], retransmit_packet, calc_checksum(chunks[retransmit_packet]))
 
             # we've received the ACK and no longer need to worry about the chunk. yay!
-            unreceived_acks.pop(ack - 1)
+
 
 
     def handle_timeout():
+        global cwnd, ssthresh, next_to_send
+
         while True:
-            for chunk_num, time_sent in unreceived_acks:
-                curr = datetime.datetime.now()
+            to_retransmit = None
 
-                if (curr - time_sent).total_seconds() < timeout:
-                    retransmit.append(chunk_num)
+            with lock:
+                now = datetime.datetime.now()
 
-            time.sleep(1)
+                if send_base in unreceived_acks:
+                    time_sent = unreceived_acks[send_base]
+
+                    if (now - time_sent).total_seconds() > timeout:
+
+                        to_retransmit = send_base
+
+                        unreceived_acks[send_base] = datetime.datetime.now()
+
+                        ssthresh = max(cwnd / 2, 1)
+                        cwnd = 1
+
+                        next_to_send = send_base
+
+            if to_retransmit is not None:
+                print("Timeout. retransmitting packet" + str(to_retransmit))
+                send_chunk(chunks[to_retransmit], to_retransmit, calc_checksum(chunks[to_retransmit]))
+
+            time.sleep(0.1)
+
 
     chunks = []
+    ack_thread = threading.Thread(target=handle_ack, daemon=True)
+    timeout_thread = threading.Thread(target=handle_timeout, daemon=True)
+    ack_thread.start()
+    timeout_thread.start()
+
+
     while True:
         chunk = file.read(1024)
         if not chunk:
@@ -100,27 +168,26 @@ with open("gistfile1.txt", "rb") as file:
 
     # congestion control
     remaining_packets = len(chunks)
-    cwnd = 1
 
 
     base_chunk = 0
-    while (remaining_packets > 0):
-        # send a chunk for each cwnd
-        for i in range(cwnd):
-            if i + base_chunk >= total_chunks:
-                break
+    seq = None
+    while (send_base < total_chunks):
+        packets_to_send = []
 
-            send_chunk(chunks[i + base_chunk], chunks[i + base_chunk], 0)
-            unreceived_acks[i+base_chunk] = datetime.datetime.now()
+        with lock:
+            # send a chunk for each cwnd
+            # int(cwnd) because it can be a float
+            while next_to_send < total_chunks and next_to_send < send_base + int(cwnd):
+                unreceived_acks[next_to_send] = datetime.datetime.now()
+                seq = next_to_send
+                next_to_send += 1
+                packets_to_send.append(seq)
 
-            print("Sent chunk number " + str(base_chunk + i))
+        for seq in packets_to_send:
+            send_chunk(chunks[seq], seq, calc_checksum(chunks[seq]))
+            print("Sent chunk number " + str(seq))
 
+        time.sleep(0.01)
 
-
-
-        print("command window = " + str(cwnd))
-        base_chunk += cwnd
-        remaining_packets = remaining_packets-cwnd
-
-        cwnd+=1
 
--- server.py
+++ server.py
@@ -1,3 +1,92 @@
+import socket
+import sys
+import struct
+import random
+import time
+
+host = "localhost"
+port = 5000
 
 
-# note: should receieve 1030 bytes
+soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    soc.bind((host, port))
+except:
+    print("Bind failed. Error : " + str(sys.exc_info()))
+    sys.exit()
+
+
+def calc_checksum(databytes):
+    sum = 0
+    for byte in databytes:
+        sum += byte
+    res = sum % 65535
+    return res
+
+
+# for detecting first part of packet
+expected_seq = 0
+batch_start_time = None
+ack_pending = False
+
+soc.settimeout(0.01)
+
+
+while True:
+    # note: should receieve 1032 bytes
+
+    header_format = "IH1024s"
+    packet_size = struct.calcsize(header_format)
+
+    try:
+        data, address = soc.recvfrom(packet_size)
+    except socket.timeout:
+        data = None
+
+    if data is not None:
+        # print(data)
+        unpacked = struct.unpack("IH1024s", data)
+
+        sequence_num = unpacked[0]
+        checksum = unpacked[1]
+        chunk = unpacked[2]
+
+        # 1. Validate checksum
+        if not (calc_checksum(chunk) == checksum):
+            print(calc_checksum(chunk))
+            print(checksum)
+            print("discarding packet... :(")
+            continue
+
+        # simulate packet loss
+        loss = random.randint(1, 10)
+        if loss == 10:
+            # dropping packet :(
+            print("Dropping packet :(" + str(sequence_num))
+            continue
+
+
+        # first packet of window
+        if (sequence_num == expected_seq):
+            # 4. write chunk to output file
+            with open("output.txt", "ab") as f:
+                f.write(chunk.rstrip(b"\x00"))
+
+            expected_seq += 1
+
+            if not ack_pending:
+                batch_start_time = time.time()
+                ack_pending = True
+        else:
+            print("out of order packet :(")
+            message = struct.pack("i", expected_seq)
+            soc.sendto(message, address)
+
+
+        # checking to see if it's the first packet in window and then applying RTT
+    if ack_pending and time.time() - batch_start_time >= 0.1:
+        # 2. Send ACKs back to client, containing sequence_num + 1
+        message = struct.pack("i", expected_seq)
+        soc.sendto(message, address)
+        ack_pending = False
